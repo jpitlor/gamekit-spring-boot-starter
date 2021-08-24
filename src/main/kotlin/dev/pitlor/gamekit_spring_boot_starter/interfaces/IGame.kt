69b053af728ab4390176113d6c8d9caf08e56962
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-08-24 09:01:32 -0400

    add some safeGet methods

--- src/main/kotlin/dev/pitlor/gamekit_spring_boot_starter/interfaces/IGame.kt
+++ src/main/kotlin/dev/pitlor/gamekit_spring_boot_starter/interfaces/IGame.kt
@@ -8,4 +8,11 @@ interface IGame<P : IPlayer> {
     val players: ArrayList<P>
     var isActive: Boolean
     var adminId: UUID
+
+    fun safeGetPlayer(id: UUID): IPlayer {
+        val player = players.find { it.id == id }
+        require(player != null) { "That player is not in this game" }
+
+        return player
+    }
 }
--- src/main/kotlin/dev/pitlor/gamekit_spring_boot_starter/interfaces/IGameRepository.kt
+++ src/main/kotlin/dev/pitlor/gamekit_spring_boot_starter/interfaces/IGameRepository.kt
@@ -1,9 +1,25 @@
 package dev.pitlor.gamekit_spring_boot_starter.interfaces
 
+import java.util.*
+
 interface IGameRepository<P : IPlayer, G : IGame<P>> {
     fun findAll(filter: (G) -> Boolean): List<G>
     fun getAllByNotStarted(): List<G>
     fun getByCode(gameCode: String): G?
     fun add(game: G)
     fun removeByCode(gameCode: String)
+
+    private fun checkGameExists(game: G?, andHasAdmin: UUID? = null): G {
+        require(game != null) { "That game does not exist" }
+        if (andHasAdmin != null) {
+            require(game.adminId == andHasAdmin) { "You are not the admin of that game" }
+        }
+
+        return game
+    }
+
+    fun safeGetByCode(gameCode: String, withAdmin: UUID? = null): G {
+        val game = getByCode(gameCode)
+        return checkGameExists(game, withAdmin)
+    }
 }
